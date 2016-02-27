10e5908c95ca54ed18d06377fa67c16a3d453391
nikhilbchilwant <nikhilbchilwant@gmail.com>
2016-02-27 20:35:05 +0530

    uploads images

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,12 +8,13 @@
  * Main module of the application.
  */
  var dashboardModule = angular.module('dashboardApp', ['ngUpload'/*'angular-google-gapi'*/]);
+ var accessToken = null;
 
  dashboardModule.controller('showFiles', function($scope/*, gapi*/){
  	var CLIENT_ID = '1094966308802-ksdjf0f4h4gblprr663ie3a8hqmcei3o.apps.googleusercontent.com';
-
- 	var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+ 	var SCOPES = ['https://www.googleapis.com/auth/drive.file'];
  	$scope.isAutherized = false;
+ 	
 
  	$scope.handleAuthClick = function() {
 
@@ -27,7 +28,7 @@
 
  		if (authResult && !authResult.error) {
  			$scope.isAutherized = true;
-
+ 			accessToken = authResult.access_token;
 
  			console.log('logged in successfully');
 			//because we are using client.js from google API
@@ -72,12 +73,84 @@
 	   	});
 	   }
 
-	   $scope.uploadFile = function(content){
-	   	var uploader = new MediaUploader({
-	   		// file: content,
-	   		// token: accessToken,
-	   	});
-	   	uploader.upload();		   	
-	   };
+
 
 	});
+
+ var uploadFile = function(content){
+ 	var uploader = new MediaUploader({
+ 		file: content,
+ 		token: accessToken,
+ 		onComplete: function(data) {
+ 		}
+ 	});
+ 	uploader.upload();		   	
+ };
+
+ dashboardModule.directive('fileDropzone', function() {
+ 	return {
+ 		restrict: 'A',
+ 		scope: {
+ 			file: '=',
+ 			fileName: '='
+ 		},
+ 		link: function(scope, element, attrs) {
+      	// console.log('triggered');
+      	var checkSize, isTypeValid, processDragOverOrEnter, validMimeTypes;
+      	processDragOverOrEnter = function(event) {
+      		if (event !== null) {
+      			event.preventDefault();
+      		}
+			// console.log(event);
+			event.originalEvent.dataTransfer.effectAllowed = 'copy';
+
+			return false;
+		};
+		validMimeTypes = attrs.fileDropzone;
+		checkSize = function(size) {
+			var _ref;
+			if (((_ref = attrs.maxFileSize) === (void 0) || _ref === '') || (size / 1024) / 1024 < attrs.maxFileSize) {
+				return true;
+			} else {
+				alert('File must be smaller than ' + attrs.maxFileSize + ' MB');
+				return false;
+			}
+		};
+		isTypeValid = function(type) {
+			if ((validMimeTypes === (void 0) || validMimeTypes === '') || validMimeTypes.indexOf(type) > -1) {
+				return true;
+			} else {
+				alert("Invalid file type.  File must be one of following types " + validMimeTypes);
+				return false;
+			}
+		};
+		element.bind('dragover', processDragOverOrEnter);
+		element.bind('dragenter', processDragOverOrEnter);
+		return element.bind('drop', function(event) {
+			var file, name, reader, size, type;
+			if (event !== null) {
+				event.preventDefault();
+			}
+			reader = new FileReader();
+			reader.onload = function(evt) {
+				if (checkSize(size) && isTypeValid(type)) {
+					return scope.$apply(function() {
+						scope.file = evt.target.result;
+						if (angular.isString(scope.fileName)) {
+							scope.fileName = name;
+							return name;
+						}
+					});
+				}
+			};
+			file = event.originalEvent.dataTransfer.files[0];
+			name = file.name;
+			type = file.type;
+			size = file.size;
+			reader.readAsDataURL(file);
+			uploadFile(file);
+			return false;
+		});
+	}
+};
+});
