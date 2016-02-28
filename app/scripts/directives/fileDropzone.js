e4fad12acf9320cd22e870afe2ccacffa353f6af
nikhilbchilwant <nikhilbchilwant@gmail.com>
2016-02-28 21:51:42 +0530

    minor refactoring

---
+++ app/scripts/directives/fileDropzone.js
@@ -0,0 +1,78 @@
+'use strict';
+
+
+angular.module('dashboardApp').directive('fileDropzone', function() {
+ 	return {
+ 		restrict: 'A',
+ 		scope: {
+ 			file: '=',
+ 			fileName: '='
+ 		},
+ 		link: function(scope, element, attrs) {
+      	// console.log('triggered');
+      	var isTypeValid, processDragOverOrEnter, validMimeTypes;
+      	processDragOverOrEnter = function(event) {
+      		if (event !== null) {
+      			event.preventDefault();      			
+      		}
+			// console.log(event);
+			event.originalEvent.dataTransfer.effectAllowed = 'copy';
+			
+			return false;
+		};
+		validMimeTypes = attrs.fileDropzone;
+
+		isTypeValid = function(type) {
+			if ((validMimeTypes === (void 0) || validMimeTypes === '') || validMimeTypes.indexOf(type) > -1) {
+				return true;
+			} else {
+				alert("Invalid file type.  File must be one of following types " + validMimeTypes);
+				return false;
+			}
+		};
+		element.bind('dragover', processDragOverOrEnter);
+		element.bind('dragenter', processDragOverOrEnter);
+		// $scope.$apply();
+		var uploadFile = function(content){
+			var uploader = new MediaUploader({
+				file: content,
+				token: accessToken,
+				onComplete: function(data) {
+					console.log('upload complete');
+				},
+				onProgress: function(data){					
+					var percentComplete = data.loaded / data.total;
+					console.log('percentComplete='+percentComplete);
+				}
+			});
+			uploader.upload();		   	
+		};
+
+		return element.bind('drop', function(event) {
+			var file, name, reader, size, type;
+			if (event !== null) {
+				event.preventDefault();
+			}
+			reader = new FileReader();
+			reader.onload = function(evt) {
+				// if (checkSize(size) && isTypeValid(type)) {
+					return scope.$apply(function() {
+						scope.file = evt.target.result;
+						if (angular.isString(scope.fileName)) {
+							scope.fileName = name;
+							return name;
+						}
+					});
+				// }
+			};
+			file = event.originalEvent.dataTransfer.files[0];
+			name = file.name;
+			type = file.type;
+			size = file.size;
+			reader.readAsDataURL(file);
+			uploadFile(file);
+			return false;
+		});
+	}
+};
+});
---
+++ app/scripts/services/driveUtil.js
@@ -0,0 +1,8 @@
+var dashboardModule = angular.module('dashboardApp', ['ngUpload']);
+
+dashboardModule.factory('driveUtil', ['', function(){
+    
+    return function name(){
+        
+    };
+}])
